dd2132183e0f25fda1b765012437267a39e2f78e
Don Okuda <don.okuda@gmail.com>
2014-05-26 15:06:02 -0700

    Add specs for border image

---
+++ spec/bourbon/css3/border_image_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'border-image' do
+  before(:all) do
+    ParserSupport.parse_file('border-image')
+  end
+
+  describe '@include border-image' do
+    context 'with arguments (url("image.png"))' do
+      it 'outputs with the vendor prefixes' do
+        expect('.image-only').to have_rule('-webkit-border-image: url("image.png")')
+        expect('.image-only').to have_rule('border-image: url("image.png")')
+        expect('.image-only').to have_rule('border-style: solid')
+      end
+    end
+
+    context 'with arguments (url("image.png"))' do
+      it 'outputs with the right vendor prefixs' do
+        expect('.image-with-width-and-style').to have_rule('-webkit-border-image: url("image.png") 20 stretch')
+        expect('.image-with-width-and-style').to have_rule('border-image: url("image.png") 20 stretch')
+        expect('.image-with-width-and-style').to have_rule('border-style: solid')
+      end
+    end
+
+    context 'with arguments (linear-gradient(45deg, orange, yellow))' do
+      it 'outputs with the right vendor prefixes' do
+        expect('.with-linear-gradients').to have_rule(
+          '-webkit-border-image: -webkit-linear-gradient(-315deg, orange, yellow)'
+        )
+        expect('.with-linear-gradients').to have_rule(
+          'border-image: linear-gradient(45deg, orange, yellow)'
+        )
+        expect('.with-linear-gradients').to have_rule(
+          'border-style: solid'
+        )
+      end
+    end
+  end
+end
